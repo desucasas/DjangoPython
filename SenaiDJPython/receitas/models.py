e6a5026a8d80c79775d86231e4f356a85e7d22b4
desucasas <debora.militani@gmail.com>
2025-12-03 19:02:06 -0300

    Django

--- SenaiDJPython/receitas/models.py
+++ SenaiDJPython/receitas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Receita(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.IntegerField()
+    criada_em = models.DateTimeField(auto_now_add=True)
+    atualizada_em = models.DateTimeField(auto_now=True) 
+    foto_receita = models.ImageField(upload_to='receitas/fotos', blank=True)
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titulo
--- SenaiDJPython/receitas/urls.py
+++ SenaiDJPython/receitas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("receita/", views.receita),
+    path("", views.home, name="receitas-home"),
+    path("receita/", views.receita, name="receitas-receita"),
 ]
